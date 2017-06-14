c883069203b49dfb2812869ebf0f702cbade48ec
zhuang10 <chenzhuang0705@gmail.com>
2017-06-14 15:12:46 -0400

    commentManager_1

---
+++ RoastMeApplication/Models/DAL/CommentsManage.cs
@@ -0,0 +1,39 @@
+﻿using RoastMeApplication.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoastMeApplication.Models.DAL
+{
+    public class CommentsManage
+    {
+        public static List<Comment> GetCommentByPictureId(int picture_id)
+        {
+            List<Comment> comment = null;
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                comment = db.Comments.Where(c=>c.PictureId == picture_id).OrderBy( c =>c.time).ToList();
+                
+            }
+            return comment;
+        }
+
+        public static void AddComment(Comment comment)
+        {
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                db.Comments.Add(comment);
+                db.SaveChanges();
+
+            }
+        }
+
+        public static List<Comment> EditCommentInfo(Comment comment)
+        {
+            List<Comment> comments = null;
+   
+            return comments;
+        }
+    }
+}
