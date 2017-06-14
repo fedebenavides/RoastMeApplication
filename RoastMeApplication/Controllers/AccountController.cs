a026ba5acdbb537ee28ea59e6dda7feb9f28a8de
fbenavid <fbenalvarez92@gmail.com>
2017-06-14 15:10:06 -0400

    REgister Login fully functional

--- RoastMeApplication/Controllers/AccountController.cs
+++ RoastMeApplication/Controllers/AccountController.cs
@@ -153,19 +153,26 @@ namespace RoastMeApplication.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Username, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
                 
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
                     Participant participant = new Participant
                     {
-                        username = user.UserName,
+                        username = model.Username,
                         email = user.Email,
                         ApplicationUserId = user.Id
                     };
                     ParticipantManager.Add(participant);
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                    
+                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+                    // Send an email with this link
+                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
                     return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
