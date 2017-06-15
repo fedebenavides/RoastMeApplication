010d3fde92bbdc08501abdb470668dc0935e040b
s0ndos <sondos.abdelatif@gmail.com>
2017-06-15 16:33:06 -0400

    Some more View stuff + added PictureController

---
+++ RoastMeApplication/Controllers/EntityControllers/PictureController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoastMeApplication.Controllers.EntityControllers
+{
+    public class PictureController : Controller
+    {
+        // GET: Picture
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
