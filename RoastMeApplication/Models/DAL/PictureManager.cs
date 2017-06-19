129d18925f97b5844b09664a18fba732ec6ceed5
zhuang10 <chenzhuang0705@gmail.com>
2017-06-19 12:49:31 -0400

    new_PictureManager

--- RoastMeApplication/Models/DAL/PictureManager.cs
+++ RoastMeApplication/Models/DAL/PictureManager.cs
@@ -31,5 +31,45 @@ namespace RoastMeApplication.Models.DAL
 
             return part.Pictures.ToList();
         }
+
+        public static void AddPicture(Picture picture)
+        {
+            using (ApplicationDbContext ctx = new ApplicationDbContext())
+            {
+                ctx.Pictures.Add(picture);
+                ctx.SaveChanges();
+            }
+        }
+
+        public static void EditPictureFlagged(Picture new_picture)
+        {
+            Picture picture = null;
+            using (ApplicationDbContext ctx = new ApplicationDbContext())
+            {
+                picture = ctx.Pictures.Where(p => p.Id == new_picture.Id).FirstOrDefault();
+                if (picture != null)
+                {
+                    picture.IsFlagged = new_picture.IsFlagged;
+                }
+                ctx.SaveChanges();
+            }
+        }
+
+        public static bool CheckImgByPath(string path)
+        {
+            Picture img = null;
+            using (ApplicationDbContext ctx = new ApplicationDbContext())
+            {
+                img = ctx.Pictures.Where(c => c.ImagePath == path).FirstOrDefault();
+            }
+            if (img == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
