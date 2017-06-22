49b5e182cf1ba3d85be55d81dd7e8ec5a3ad7fec
fbenavid <fbenalvarez92@gmail.com>
2017-06-22 14:15:29 -0400

    flags management and comments sorting

--- RoastMeApplication/Models/DAL/PictureManager.cs
+++ RoastMeApplication/Models/DAL/PictureManager.cs
@@ -112,5 +112,21 @@ namespace RoastMeApplication.Models.DAL
             List<Picture> flaggedPics = allPics.Where(p => p.IsFlagged == true).ToList();
             return flaggedPics;
         }
+
+        public static void DeletePicture(Picture new_picture)
+        {
+            using (ApplicationDbContext ctx = new ApplicationDbContext())
+            {
+                Picture picture = ctx.Pictures.Where(p => p.Id == new_picture.Id).FirstOrDefault();
+
+                if (picture != null)
+                {
+                    //delete
+                    ctx.Pictures.Remove(picture);
+                }
+                //save changes
+                ctx.SaveChanges();
+            }
+        }
     }
 }
